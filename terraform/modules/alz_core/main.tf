370e347957cf161c431b1f6a1554447ff4a91737
Saboor Jamal <saboorjamal@FVFFX9KSQ6LR.local>
2025-12-10 22:13:32 -0500

    Add azurerm provider aliases to ALZ module

--- terraform/modules/alz_core/main.tf
+++ terraform/modules/alz_core/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+      configuration_aliases = [
+        azurerm.connectivity,
+        azurerm.management,
+      ]
+    }
+  }
+}
+
+
 locals {
   deploy           = var.enabled
   mg               = try(var.management_groups, {})
